98dd54ecdfa8e261d924f52dab40509c3788d088
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-05 05:37:57 -0700

    Update script.

--- update_godot_fire.sh
+++ update_godot_fire.sh
@@ -4,19 +4,15 @@ echo -e "Checkout remotes"
 git remote rm SaracenOne
 git remote add SaracenOne https://github.com/SaracenOne/godot
 git fetch SaracenOne
-
 git remote rm lyuma
 git remote add lyuma https://github.com/lyuma/godot
 git fetch lyuma
-
 git remote rm godotengine
 git remote add godotengine https://github.com/godotengine/godot
 git fetch godotengine
-
 git remote rm "extended-fire-godot"
 git remote add "extended-fire-godot" git@github.com:godot-extended-libraries/godot-fire.git
 git fetch "extended-fire-godot"
-
 git remote rm fire
 git remote add fire git@github.com:fire/godot.git
 git fetch fire
