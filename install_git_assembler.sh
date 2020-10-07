45d726f2d08ae1c1d3f529c4ae9ab6f8f1248ed6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-07 05:03:36 -0700

    Install git-assembler.

--- install_git_assembler.sh
+++ install_git_assembler.sh
@@ -1,4 +1,4 @@
-sudo curl -L https://gitlab.com/wavexx/git-assembler/-/raw/master/git-assembler -o /usr/local/bin/git-assembler
-sudo chmod +x /usr/local/bin/git-assembler
+curl -L https://gitlab.com/wavexx/git-assembler/-/raw/master/git-assembler -o git-assembler
+chmod +x git-assembler
 git config --global alias.as assembler
 git config rerere.enabled true
