8360b4ae276b3fcd972181a116bb19f2b9bd7da7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-10 17:39:07 -0700

    Typo.

--- update_godot_fire.sh
+++ update_godot_fire.sh
@@ -16,10 +16,9 @@ git remote add godotengine https://github.com/godotengine/godot
 git fetch godotengine
 git remote rm "extended-fire-godot" || true
 git remote add "extended-fire-godot" git@github.com:godot-extended-libraries/godot-fire.git
-git fetch "extended-fire-godot"
-git remote rm fire || true
-git remote add fire git@github.com:fire/godot.git
-git fetch RevoluPowered
+git remote rm extended-fire-godot || true
+git remote add extended-fire-godot git@github.com:fire/godot.git
+git fetch extended-fire-godot
 git remote rm RevoluPowered || true
 git remote add RevoluPowered https://github.com/RevoluPowered/godot.git
 git fetch RevoluPowered
