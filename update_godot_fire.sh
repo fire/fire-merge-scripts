60d971cd89786eb2bd703c88f8f9cdf6dbd8484c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-10 17:36:47 -0700

    Add safety steps.

--- update_godot_fire.sh
+++ update_godot_fire.sh
@@ -5,29 +5,29 @@ set -e
 git stash
 
 echo -e "Checkout remotes"
-git remote rm SaracenOne
+git remote rm SaracenOne || true
 git remote add SaracenOne https://github.com/SaracenOne/godot
 git fetch SaracenOne
-git remote rm lyuma
+git remote rm lyuma || true
 git remote add lyuma https://github.com/lyuma/godot
 git fetch lyuma
-git remote rm godotengine
+git remote rm godotengine || true
 git remote add godotengine https://github.com/godotengine/godot
 git fetch godotengine
-git remote rm "extended-fire-godot"
+git remote rm "extended-fire-godot" || true
 git remote add "extended-fire-godot" git@github.com:godot-extended-libraries/godot-fire.git
 git fetch "extended-fire-godot"
-git remote rm fire
+git remote rm fire || true
 git remote add fire git@github.com:fire/godot.git
 git fetch RevoluPowered
-git remote rm RevoluPowered
+git remote rm RevoluPowered || true
 git remote add RevoluPowered https://github.com/RevoluPowered/godot.git
 git fetch RevoluPowered
 
 
 echo -e "Work"
 git checkout master --force
-git branch -D "extended-fire"
+git branch -D "extended-fire" || true
 ./thirdparty/git-assembler -av
 git checkout "extended-fire" -f
 git push -u -f "extended-fire-godot" extended-fire
