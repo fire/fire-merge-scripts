56d79fc32f8f9ffd312bebafcce8028587fbbc5c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-05-19 07:34:02 -0700

    Ban rerere.

--- install_git_assembler.sh
+++ install_git_assembler.sh
@@ -1,3 +1,4 @@
 curl -L https://gitlab.com/wavexx/git-assembler/-/raw/master/git-assembler -o thirdparty/git-assembler
 chmod +x thirdparty/git-assembler
-git config rerere.enabled true
+# ban rerere
+# git config rerere.enabled true
